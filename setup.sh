dc023b5055253149ce6cef67b8e499086d14b59f
Rahul Meena <100401459+Evilmove@users.noreply.github.com>
2025-06-01 22:07:11 +0530

    setting up that no need to ask permission again and again

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 echo "update system"
-sudo pacman -Sy
+sudo pacman -Sy -y
 clear
 echo "upgrading"
-sudo pacman -Syu
+sudo pacman -Syu -y
 clear
 echo "installing firmware"
-sudo pacman -Sy sof-firmware
-sudo pacman -Sy linux-firmware-qlogic
+sudo pacman -Sy sof-firmware -y
+sudo pacman -Sy linux-firmware-qlogic -y
 clear
 echo "installing git"
-sudo pacman -Sy git
+sudo pacman -Sy git -y
 clear
 echo "enable bluetooth"
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 clear
 echo "installing firefox and neofetch"
-sudo pacman -S firefox
-sudo pacman -S neofetch
+sudo pacman -S firefox -y
+sudo pacman -S neofetch -y
 clear
 #to check nvidia status
 echo "check nvidia status"
-nvidia-smi
+nvidia-smi -y
